b56a97f425416ba2947d18936d4fc641682ac718
Dennis Kyalo <kyalodennis1738@gmail.com>
2025-07-02 15:39:48 +0300

    final

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@
 from flask import request, session
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
-
+from flask_migrate import Migrate
 from config import app, db, api
 from models import User, Recipe
 
